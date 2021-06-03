71cd149a1575c94317484c2fbe59891eedf250ed
Kate Kim <katehrkim@gmail.com>
2021-06-03 10:44:46 -0400

    Updated from part 1

--- boggle_board.py
+++ boggle_board.py
@@ -3,11 +3,36 @@ import random
 import string
 class BoggleBoard:
     def __init__(self):
-        self.board = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']]
+        self.board = [
+            ['_','_','_','_'],
+            ['_','_','_','_'],
+            ['_','_','_','_'],
+            ['_','_','_','_']]
+        self.dice = [
+            'AAEEGN',
+            'ELRTTY',
+            'AOOTTW',
+            'ABBJOO',
+            'EHRTVW',
+            'CIMOTU',
+            'DISTTY',
+            'EIOSST',
+            'DELRVY',
+            'ACHOPS',
+            'HIMNQU',
+            'EEINSU',
+            'EEGHNW',
+            'AFFKPS',
+            'HLNNRZ',
+            'DEILRX'
+        ]
     def shake(self):
+        random.shuffle(self.dice)
         for i, list in enumerate(self.board):
             for j, elem in enumerate(list):
-                random_letter = random.choice(string.ascii_uppercase)
+                # random_letter = random.choice(string.ascii_uppercase)
+                random_index = random.randint(0,5)
+                random_letter = self.dice[i][random_index]
                 if random_letter == 'Q':
                     random_letter = 'Qu'
                 self.board[i][j] = random_letter
